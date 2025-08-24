c044764771303aaa13da0a9eed37d45ebbef7658
Daniel Ogirev <marsik025@gmail.com>
2025-08-24 14:03:36 +0300

    feat: configure vite & docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,33 @@
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        server: {
+            port: env.EXPOSE_VITE_PORT,
+            hmr: {
+                host: 'localhost',
             },
-        }),
-    ],
+        },
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                ssr: 'resources/js/ssr.ts',
+                refresh: true,
+            }),
+            tailwindcss(),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+    }
 });
